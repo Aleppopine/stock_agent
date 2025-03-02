6cc15c73e439488f44259cd5b001569d5a5620c1
Austin <cdbarnhil2@gmail.com>
2025-03-02 10:17:38 -0500

    Initial commit for Heroku deployment

--- stock_agent.py
+++ stock_agent.py
@@ -174,4 +174,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Heroku's dynamic port
+    app.run(host="0.0.0.0", port=port)  # Don't use debug=True in production
